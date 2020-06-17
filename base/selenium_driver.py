478084a4bbda180df3152b7cc86ab185dfa1a6a7
Youngsun Paik <youngsunpaik@gmail.com>
2020-06-17 10:45:21 -0700

    selenium_driver and updated teststatus

---
+++ base/selenium_driver.py
@@ -0,0 +1,39 @@
+from utilities.custom_logger import custom_logger
+import logging
+import time
+import os
+
+
+class SeleniumDriver:
+    log = custom_logger(logging.DEBUG)
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def get_screenshot(self, message):
+        file_name = message + "_" + str(round(time.time() * 1000)) + ".png"
+        screenshot_dir = "../screenshots/"
+        relative_file_name = screenshot_dir + file_name
+        current_dir = os.path.dirname(__file__)
+        destination_file = os.path.join(current_dir, relative_file_name)
+        destination_dir = os.path.join(current_dir, screenshot_dir)
+
+        try:
+            if not os.path.exists(destination_dir):
+                os.makedirs(destination_dir)
+            self.driver.save_screenshot(destination_file)
+            self.log.info("Screenshot saved to directory: " + destination_file)
+        except:
+            self.log.error("### Exception Occurred when taking screenshot")
+
+    def is_element_present(self, element):
+        try:
+            if element is not None:
+                self.log.info("Element present: " + element)
+                return True
+            else:
+                self.log.info("Element not present")
+                return False
+        except:
+            print("Element not found")
+            return False
--- utilities/teststatus.py
+++ utilities/teststatus.py
@@ -1,10 +1,14 @@
 from utilities.custom_logger import custom_logger
+from base.selenium_driver import SeleniumDriver
 import logging
 
 
-class TestStatus:
+class TestStatus(SeleniumDriver):
     log = custom_logger(logging.INFO)
-    result_list = []
+
+    def __init__(self, driver):
+        super(TestStatus, self).__init__(driver)
+        self.result_list = []
 
     def set_result(self, result, message):
         try:
@@ -15,12 +19,15 @@ class TestStatus:
                 else:
                     self.result_list.append("FAIL")
                     self.log.error("### VERIFICATION FAILED :: + " + message)
+                    self.get_screenshot(message)
             else:
                 self.result_list.append("FAIL")
                 self.log.error("### VERIFICATION FAILED :: + " + message)
+                self.get_screenshot(message)
         except:
             self.result_list.append("FAIL")
             self.log.error("### EXCEPTION Occurred")
+            self.get_screenshot(message)
 
     def mark(self, result, message):
         self.set_result(result, message)
