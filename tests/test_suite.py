0615f77d658b180ccfa5168aa676c5acd55aeb3d
Youngsun Paik <youngsunpaik@gmail.com>
2020-11-06 13:26:49 -0800

    updated test suite

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,10 +1,18 @@
 import unittest
 from tests.login_test import LoginTest
 from tests.front_page_test import FrontPageTest
+from tests.search_test import SearchTest
+from tests.cart_test import CartTest
+from tests.checkout_test import CheckoutTest
+from tests.contact_test import ContactTest
 
 login_test = unittest.TestLoader().loadTestsFromTestCase(LoginTest)
 front_page_test = unittest.TestLoader().loadTestsFromTestCase(FrontPageTest)
+search_test = unittest.TestLoader().loadTestsFromTestCase(SearchTest)
+cart_test = unittest.TestLoader().loadTestsFromTestCase(CartTest)
+checkout_test = unittest.TestLoader().loadTestsFromTestCase(CheckoutTest)
+contact_test = unittest.TestLoader().loadTestsFromTestCase(ContactTest)
 
-smoke_test = unittest.TestSuite([login_test, front_page_test])
+smoke_test = unittest.TestSuite([login_test, front_page_test, search_test, cart_test, checkout_test, contact_test])
 
 unittest.TextTestRunner(verbosity=2).run(smoke_test)
